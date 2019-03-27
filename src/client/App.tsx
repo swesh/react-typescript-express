a2b1067b8f97656b697ab62e897dfb63d9f92c33
swesh <wesh.mishra123@gmail.com>
2019-03-28 04:21:38 +0530

    Adding routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,39 @@
 import * as React from 'react';
 import './app.css';
+import About from './components/About';
+import Home from './components/HomeComponent';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 export default class App extends React.Component {
-  
+
   componentDidMount() {
     fetch('/api/getUsername')
       .then(res => res.json())
-      .then(user => {console.log(user) });
+      .then(user => { console.log("From API--->", user) });
   }
 
   render() {
     return (
       <div>
-        <h1> Hello react </h1>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/home"> Home </Link>
+              </li>
+              <li>
+                <Link to="/about"> About </Link>
+              </li>
+            </ul>
+            <Route exact path='/home' component={Home}></Route>
+            <Route exact path='/about' component={About}></Route>
+          </div>
+        </Router>
+
       </div>
     );
   }
