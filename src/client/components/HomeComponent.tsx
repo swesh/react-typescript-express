f2a109157e047b3ac5107cb958cf78070ca5577d
swesh <wesh.mishra123@gmail.com>
2019-03-28 04:06:05 +0530

    Adding router

---
+++ src/client/components/HomeComponent.tsx
@@ -0,0 +1,8 @@
+
+import * as React from 'react';
+
+function HomeComponent (){
+ return <h1>Home Component</h1>
+}
+
+export default HomeComponent;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,17 @@
 import * as express from 'express';
 import * as os from 'os';
+import * as path from 'path';
 
 const app = express();
 
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static('dist'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname,  'index.html'));
+  });
+
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
